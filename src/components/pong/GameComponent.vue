3b496ebd7639014189bf0a625b4c56c6335af9b3
Adrien Cassar <adricass@live.fr>
2024-01-16 22:48:20 +0100

    player up and down

--- src/components/pong/GameComponent.vue
+++ src/components/pong/GameComponent.vue
@@ -2,10 +2,33 @@
 import { usePongStore } from '@/stores/pong';
 import RacketComponent from './RacketComponent.vue';
 import { storeToRefs } from 'pinia';
+import { onMounted, ref } from 'vue';
 
 const pongStore = usePongStore();
 
 const { playerCoords, enemyCoords } = storeToRefs(pongStore);
+const { setPlayerKey, movePlayer } = pongStore;
+const pressingDown = ref<Set<string>>(new Set());
+
+onMounted(() => {
+	setInterval(() => {
+		movePlayer();
+	}, 10);
+});
+
+document.addEventListener('keydown', (e) => {
+	pressingDown.value.add(e.key);
+
+	if (e.key === "ArrowDown" || e.key === "ArrowUp")
+		setPlayerKey(e.key);
+});
+
+document.addEventListener('keyup', (e) => {
+	pressingDown.value.delete(e.key);
+	if (pressingDown.value.size === 0)
+		setPlayerKey(null);
+
+});
 
 </script>
 
--- src/stores/pong.ts
+++ src/stores/pong.ts
@@ -1,12 +1,16 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
-import { RACKET_WIDTH, SPACE_SIDES } from '@/components/data/PongData';
+import { RACKET_SPEED, RACKET_WIDTH, SPACE_SIDES } from '@/components/data/PongData';
 
 type Coords = { x: number, y: number}
+type PlayerKeyType = null | undefined | "ArrowUp" | "ArrowDown"
 
 export const usePongStore = defineStore('pong', () => {
-	const playerCoords = ref({ x: 10, y: 0 });
+	const playerCoords = ref({ x: SPACE_SIDES, y: 0 });
 	const enemyCoords = ref({ x: window.innerWidth - (RACKET_WIDTH + SPACE_SIDES), y: 0 });
+	const playerKey = ref<PlayerKeyType>();
+
+	const setPlayerKey = (key: PlayerKeyType) => playerKey.value = key;
 
 	const setPlayerCoords = (coords: Coords) => {
 		playerCoords.value = coords;
@@ -16,6 +20,37 @@ export const usePongStore = defineStore('pong', () => {
 		enemyCoords.value = coords;
 	};
 
+	const movePlayer = () => {
+		if (playerKey.value) {
+			switch (playerKey.value) {
+				case "ArrowUp":
+					moveUp("player");
+					break;
+				case "ArrowDown":
+					moveDown("player");
+					break;
+				default:
+					break;
+			}
+		}
+	};
 
-	return { playerCoords, enemyCoords, setPlayerCoords, setEnemyCoords };
+	const moveDown = (type: 'player' | 'enemy') => {
+		if (type === 'player') {
+			setPlayerCoords({ x: playerCoords.value.x, y: playerCoords.value.y + RACKET_SPEED });
+		} else if (type === 'enemy') {
+			setEnemyCoords({ x: enemyCoords.value.x, y: enemyCoords.value.y + RACKET_SPEED });
+		}
+	};
+
+	const moveUp = (type: 'player' | 'enemy') => {
+		if (type === 'player') {
+			setPlayerCoords({ x: playerCoords.value.x, y: playerCoords.value.y - RACKET_SPEED });
+		} else if (type === 'enemy') {
+			setEnemyCoords({ x: enemyCoords.value.x, y: enemyCoords.value.y - RACKET_SPEED });
+		}
+	};
+
+
+	return { playerCoords, enemyCoords, setPlayerCoords, setEnemyCoords, playerKey, setPlayerKey, movePlayer };
 });
