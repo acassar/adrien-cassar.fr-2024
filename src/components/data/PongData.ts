9ef386d2f16e3d31697429276892f4c647d22573
Adrien Cassar <adricass@live.fr>
2024-01-17 14:33:03 +0100

    Acceleration on racket edges

--- src/components/data/PongData.ts
+++ src/components/data/PongData.ts
@@ -3,6 +3,6 @@ const RACKET_HEIGHT = 60; //in px
 const SPACE_SIDES = 10; //in px
 const RACKET_SPEED = 2; //in px
 const BALL_SIZE = 10; //in px
-const BALL_SPEED = 3; //in px
+const BALL_SPEED = 2; //in px
 
 export {RACKET_WIDTH, RACKET_HEIGHT, SPACE_SIDES, RACKET_SPEED, BALL_SIZE, BALL_SPEED};
