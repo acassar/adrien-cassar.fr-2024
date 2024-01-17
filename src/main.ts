9c724129cf42e0f9827d5787ec2be542ca5390bb
Adrien Cassar <adricass@live.fr>
2024-01-17 18:42:46 +0100

    i18n added

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+import fr from './assets/i18n/fr';
+import en from './assets/i18n/en';
 
-app.use(createPinia())
-app.use(router)
+const messages = {
+	en: en,
+	fr: fr
+};
 
-app.mount('#app')
+const i18n = createI18n({
+	legacy: false, // you must set `false`, to use Composition API
+	locale: 'fr', // set locale
+	fallbackLocale: 'en', // set fallback locale
+	messages, // set locale messages
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(i18n);
+app.use(router);
+
+app.mount('#app');
