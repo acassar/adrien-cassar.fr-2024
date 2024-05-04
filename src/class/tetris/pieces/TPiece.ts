dc2d829dcd8efa5dbbf6a825c79662b8a61a42f3
Adrien Cassar <adricass@live.fr>
2024-05-04 15:20:48 +0200

    comments in Tpiece class

--- src/class/tetris/pieces/TPiece.ts
+++ src/class/tetris/pieces/TPiece.ts
@@ -2,8 +2,16 @@ import type { GridSize } from "@/types/tetris/grid";
 import { Piece } from "../piece";
 import { PieceBlock } from "../pieceBlock";
 
+/**
+ * Represents a specific type of Tetris piece.
+ * @extends Piece
+ */
 export class TPiece extends Piece {
 
+	/**
+     * Returns the future positions of the piece based on its current rotation.
+     * @returns An array of arrays, where each inner array contains a PieceBlock object and its new position after rotation.
+     */
 	getFuturePositions(): [PieceBlock, number][] {
 		switch (this.actualRotation) {
 			case 'top':
@@ -16,13 +24,17 @@ export class TPiece extends Piece {
 				return [
 					[this.pieceBlocks[0], this.pieceBlocks[0].position - (this.gridSize.x + 1)],
 					[this.pieceBlocks[2], this.pieceBlocks[2].position + (this.gridSize.x + 1)],
-					[this.pieceBlocks[3], this.pieceBlocks[3].position + (this.gridSize.x - 1)],
+					[this.pieceBlocks[3], this.pieceBlocks[3].position + (this.gridSize.x - 1)]
 				];
-			default: throw Error("Unknown rotation: " + this.actualRotation);
-
+			default:
+				throw Error("Unknown rotation: " + this.actualRotation);
 		}
 	}
 
+	/**
+     * Checks if the piece can rotate from the top.
+     * @returns True if the piece can rotate from the top, false otherwise.
+     */
 	canRotateFromTop(): boolean {
 		if (this.pieceBlocks[2].isFarTop(this.gridSize.x)) {
 			this.fall();
@@ -30,6 +42,10 @@ export class TPiece extends Piece {
 		return true;
 	}
 
+	/**
+     * Checks if the piece can rotate from the right.
+     * @returns True if the piece can rotate from the right, false otherwise.
+     */
 	canRotateFromRight(): boolean {
 		if (this.pieceBlocks[3].isFarRight(this.gridSize.x)) {
 			this.move(-1);
@@ -37,14 +53,21 @@ export class TPiece extends Piece {
 		return true;
 	}
 
+	/**
+     * Checks if the piece can rotate from the bottom.
+     * @returns True if the piece can rotate from the bottom, false otherwise.
+     */
 	canRotateFromBottom(): boolean {
 		if (!this.pieceBlocks[0].isFarBottom(this.gridSize)) {
-
 			return true;
 		}
 		return false;
 	}
 
+	/**
+     * Checks if the piece can rotate from the left.
+     * @returns True if the piece can rotate from the left, false otherwise.
+     */
 	canRotateFromLeft(): boolean {
 		if (this.pieceBlocks[0].isFarLeft(this.gridSize.x)) {
 			this.move(1);
@@ -52,6 +75,10 @@ export class TPiece extends Piece {
 		return true;
 	}
 
+	/**
+     * Constructor for the TPiece class.
+     * @param gridSize The size of the grid.
+     */
 	constructor(gridSize: GridSize) {
 		const pieceOrigin = Math.floor(gridSize.x / 2 - 1);
 		super([new PieceBlock(pieceOrigin), new PieceBlock(pieceOrigin + 1), new PieceBlock(pieceOrigin + 2), new PieceBlock(pieceOrigin + 1 + gridSize.x)], gridSize);
